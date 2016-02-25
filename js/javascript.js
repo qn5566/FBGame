6980f2c7a66ee19963943fdc316a9ebc740e8bb9
Hsu <sohotguys@gmail.com>
2016-02-25 13:53:11 +0800

    11
    11

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+    $(".css_btn_class").on("click", function (e) {
+        alert(5);
+
+        $.ajax({
+            url: "push_trick.php",
+            type: "get",
+            data: $("#push_content").serialize(),
+            success: function (msg) {
+
+                $('#ffff').html(msg);
+
+            }
+        })
+
+
+    })
+
+})
