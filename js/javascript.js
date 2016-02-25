6022fd1d6d834de06bf753df1f86cfbfaf1935e9
Hsu <sohotguys@gmail.com>
2016-02-25 14:44:10 +0800

    joey3

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
     $(".css_btn_class").on("click", function (e) {
-        alert(5);
+        alert("果然是個聰明人!");
 
         $.ajax({
             url: "push_trick.php",
